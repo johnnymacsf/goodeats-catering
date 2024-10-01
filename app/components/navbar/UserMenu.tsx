c5807f9d0442de163d2b154d8bf3ff78981d78be
John Gill <johnnymacsf@outlook.com>
2024-10-01 13:32:10 -0700

    Added favorite restaurant listings menu item

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -58,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                             <>
                             <MenuItem onClick={()=>router.push("/caterings")} label="My caterings"/>
                             <MenuItem onClick={()=>router.push("/reservations")} label="My reservations"/>
-                            <MenuItem onClick={()=>{}} label="My favorites"/>
+                            <MenuItem onClick={()=>{router.push("/favorites")}} label="My favorites"/>
                             <MenuItem onClick={()=>{}} label="My restaurant listings"/>
                             <MenuItem onClick={listModal.onOpen} label="List my restaurant"/>
                             <hr />
