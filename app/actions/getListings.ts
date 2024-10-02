817290e1b88247f7f7caa1db86ea5ccc96a1d768
John Gill <130315364+johnnymacsf@users.noreply.github.com>
2024-10-02 12:39:01 -0700

    Update getListings.ts

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,53 +1,9 @@
 import prisma from '@/app/libs/prismadb';
 
-export interface IListingsParams{
-    userId?: string;
-    guestCount?: number;
-    startDate?: string;
-    endDate?: string;
-    category?: string;
-}
 
-export default async function getListings(
-    params: IListingsParams
-){
+export default async function getListings(){
     try{
-        const{ userId, guestCount, startDate, endDate, category} = params;
-
-        let query: any = {};
-
-        if(userId){
-            query.userId = userId;
-        }
-        if(category){
-            query.category = category;
-        }
-        if(guestCount){
-            query.guestCount = {
-                gte: +guestCount
-            }
-        }
-        if(startDate && endDate){
-            query.NOT = {
-                reservations: {
-                    some: {
-                        OR: [
-                            {
-                                endDate: { gte: startDate},
-                                startDate: { lte: startDate}
-                            },
-                            {
-                                startDate: { lte: endDate},
-                                endDate: {gte: endDate}
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-
         const listings = await prisma.listing.findMany({
-            where: query,
             orderBy:{
                 createdAt: 'desc'
             }
